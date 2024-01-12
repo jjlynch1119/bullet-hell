ed779a35055b4a87f42231fb72647c77317ca0a5
jlynch1119 <144484055+jlynch1119@users.noreply.github.com>
2024-01-11 23:14:51 -0600

    Capitalize main.cpp name and create initial window

--- BulletHell/Game.cpp
+++ BulletHell/Game.cpp
@@ -13,16 +13,16 @@ Game::~Game()
 void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
     int flags = 0;
-
     if (fullscreen)
     {
         flags = SDL_WINDOW_FULLSCREEN;
     }
-    
-    if (SDL_Init(SDL_INIT_EVERYTHING == 0))
+
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
-        std::cout <<"Subsystems Initialized...\n";
-        
+        std::cout << "Subsystems Initialized!...\n";
+
         window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
         if (window)
         {
@@ -32,11 +32,12 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
         renderer = SDL_CreateRenderer(window, -1, 0);
         if (renderer)
         {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             std::cout << "Renderer created!\n";
         }
 
         isRunning = true;
-    }  
+    }
     else
     {
         isRunning = false;
@@ -45,7 +46,18 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 void Game::handleEvents()
 {
+    SDL_Event event;
+    SDL_PollEvent(&event);
 
+    switch (event.type)
+    {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+
+        default:
+            break;
+    }
 }
 
 void Game::update()
@@ -55,12 +67,17 @@ void Game::update()
 
 void Game::render()
 {
-
+    SDL_RenderClear(renderer);
+    //this is where we would add stuff to render
+    SDL_RenderPresent(renderer);
 }
 
 void Game::clean()
 {
-    
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    std::cout << "Game Cleaned\n";
 }
 
 bool Game::running()
